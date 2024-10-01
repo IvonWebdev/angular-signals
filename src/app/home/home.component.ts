5b42717170c7e8a54dd31a7ee6e0b021cced1254
Vladimir Ivchenko <ivon.webdev@gmail.com>
2024-10-01 14:36:43 +0300

    CRUD operations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,60 @@
-import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
-import {CoursesService} from "../services/courses.service";
-import {Course, sortCoursesBySeqNo} from "../models/course.model";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
-import {MatDialog} from "@angular/material/dialog";
-import {MessagesService} from "../messages/messages.service";
-import {catchError, from, throwError} from "rxjs";
-import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
+import { afterNextRender, afterRender, Component, computed, effect, inject, Injector, signal } from '@angular/core';
+import { CoursesServiceWithFetch } from '../services/courses-fetch.service';
+import { CoursesService } from '../services/courses.service';
+import { Course, sortCoursesBySeqNo } from '../models/course.model';
+import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { CoursesCardListComponent } from '../courses-card-list/courses-card-list.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MessagesService } from '../messages/messages.service';
+import { catchError, from, throwError } from 'rxjs';
+import { toObservable, toSignal, outputToObservable, outputFromObservable } from '@angular/core/rxjs-interop';
 
 @Component({
-  selector: 'home',
-  standalone: true,
-  imports: [
-    MatTabGroup,
-    MatTab,
-    CoursesCardListComponent
-  ],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'home',
+    standalone: true,
+    imports: [MatTabGroup, MatTab, CoursesCardListComponent],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+    #courses = signal<Course[]>([]);
 
+    beginnerCourses = computed(() => this.#courses().filter((course) => course.category === 'BEGINNER'));
+    advancedCourses = computed(() => this.#courses().filter((course) => course.category === 'ADVANCED'));
+
+    // private coursesFService = inject(CoursesServiceWithFetch);
+    private coursesService = inject(CoursesService);
+
+    constructor() {
+        console.log('HomeComponent constructor');
+
+        effect(() => {
+            console.log('Beggining courses:', this.beginnerCourses());
+            console.log('Advanced courses:', this.advancedCourses());
+        });
+        afterNextRender(() => {
+            console.log('HomeComponent afterNextRender');
+            this.loadFCourses().then(() => console.log(`Courses loaded:`, this.#courses()));
+        });
+
+        // afterRender(() => {
+        //     console.log('HomeComponent afterRender');
+        // });
+    }
+
+    ngOnInit() {
+        console.log('HomeComponent ngOnInit');
+
+        // this.loadFCourses().then(() => console.log(`Courses loaded:`, this.courses()));
+    }
+
+    async loadFCourses() {
+        try {
+            // const courses = await this.coursesFService.getAllCourses();
+            const courses = await this.coursesService.getAllCourses();
+            this.#courses.set(courses.sort(sortCoursesBySeqNo));
+        } catch (error) {
+            console.error(error);
+        }
+    }
 }
--- src/app/services/courses-fetch.service.ts
+++ src/app/services/courses-fetch.service.ts
@@ -1,14 +1,54 @@
-import {Injectable} from "@angular/core";
-import {environment} from "../../environments/environment";
-import {Course} from "../models/course.model";
-
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Course } from '../models/course.model';
+import { deleteCourse } from '../../../server/delete-course.route';
 
 @Injectable({
-  providedIn: "root"
+    providedIn: 'root',
 })
 export class CoursesServiceWithFetch {
+    env = environment;
 
-  env = environment;
+    async getAllCourses(): Promise<Course[]> {
+        const response = await fetch(`${this.env.apiRoot}/courses`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const payload = await response.json();
+        return payload.courses;
+    }
 
+    async createCourse(course: Partial<Course>): Promise<Course> {
+        const response = await fetch(`${this.env.apiRoot}/courses`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(course),
+        });
 
+        return response.json();
+    }
+
+    async saveCourse(courseId: string, course: Partial<Course>): Promise<Course> {
+        const response = await fetch(`${this.env.apiRoot}/courses/${courseId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(course),
+        });
+
+        return await response.json();
+    }
+
+    async deleteCourse(courseId: string): Promise<void> {
+        const response = await fetch(`${this.env.apiRoot}/courses/${courseId}`, {
+            method: 'DELETE',
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+    }
 }
